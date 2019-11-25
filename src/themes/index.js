92e45c6806459f83b0d9fbe562b9dc0130df7b18
Paper Coding <johnnguyen2202@outlook.com>
2019-11-25 15:44:21 +0700

    Dev-NewHome-NewUI: Update themes - add dark mode theme and light mode theme colors

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,26 +1,55 @@
 import {Platform, Dimensions} from 'react-native';
+import {DefaultTheme} from 'react-native-paper';
+
+export const DarkMode = {
+  ...DefaultTheme,
+  dark: false,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#B28BF5',
+    onPrimary: '#BB94F8',
+    secondary: '#62D7C6',
+    onSecondary: '#121212',
+    background: '#121212',
+    onBackground: '',
+    surface: '#323232',
+    onSurface: '#FFFFFF',
+    disabled: '',
+    placeholder: '',
+    backdrop: '',
+    bottomTabBar: '#323232',
+    activeBottomTabBar: '#FFFFFF',
+    inactiveBottomTabBar: '#6b778d',
+  },
+};
+
+export const LightMode = {
+  ...DefaultTheme,
+  dark: true,
+  mode: 'exact',
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#5723E5',
+    onPrimary: '#FFFFFF',
+    secondary: '#62D7C6',
+    onSecondary: '#000000',
+    background: '#FFFFFF',
+    onBackground: '#000000',
+    surface: '#FFFFFF',
+    onSurface: '#000000',
+    disabled: '',
+    placeholder: '',
+    backdrop: '',
+    bottomTabBar: '#5723E5',
+    activeBottomTabBar: '#FFFFFF',
+    inactiveBottomTabBar: '#6b778d',
+  },
+};
+
 // const logo = require('./assets/logo.jpg');
 // const logoLight = require('./assets/logoLight.png');
 
 const dimensions = Dimensions.get('window');
-
-const primary = 'rgba(18, 25, 50, 1)';
-const primaryLight = 'rgba(27, 37, 77, 1)';
-const primaryDark = 'rgba(14, 20, 43, 1)';
-const primaryOpaque = opacity => `rgba(18, 25, 50, ${opacity})`;
-
-const primaryText = 'white';
-
-const highlight = '#61dafb';
-
-const colors = {
-  primary,
-  highlight,
-  primaryLight,
-  primaryDark,
-  primaryOpaque,
-  primaryText,
-};
 
 const typography = {
   primary: Platform.select({
@@ -34,4 +63,4 @@ const typography = {
 };
 
 // export {colors, typography, dimensions, logo, logoLight};
-export {colors, typography, dimensions};
+export {typography, dimensions};

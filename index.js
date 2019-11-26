4c14bc05ba8a80fe1aadcf3a52e87da7c92aea6f
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-11-27 01:09:21 +0700

    Dev - Setup - Redux: Update index - import reactotron config

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
+import './src/configs/ReactotronConfig';
 import {AppRegistry} from 'react-native';
-
 import {name as appName} from './app.json';
 import App from './src/screens/App';
 

69d1a59edabe3213d3ca908d581587d538d3efee
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-11-27 14:52:03 +0700

    Dev - Setup - Redux: initial setup store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,26 +1,23 @@
-import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import Reactotron from 'reactotron-react-native';
+import * as rootReducer from '../reducers';
+import {addTodo, completeTodo} from '../actions';
+import {getVisibleTodos} from '../selectors';
 
-function counter(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
+const combinedReducer = combineReducers(rootReducer);
 
-// Create a Redux store holding the state of your app.
-// Its API is { subscribe, dispatch, getState }.
-let store = createStore(counter, Reactotron.createEnhancer());
+let store = createStore(combinedReducer, Reactotron.createEnhancer());
 
-store.dispatch({type: 'INCREMENT'});
-// 1
-store.dispatch({type: 'INCREMENT'});
-// 2
-store.dispatch({type: 'DECREMENT'});
-// 1
+store.subscribe(() => {
+  console.tron.log('Store:', store.getState());
+  console.tron.log('Test reselect: ', getVisibleTodos(store.getState()));
+});
+
+store.dispatch(addTodo('Setup Redux'));
+store.dispatch(addTodo('Go to gym at 5:00 PM'));
+
+setTimeout(() => {
+  store.dispatch(completeTodo(0));
+}, 2000);
 
 export default store;

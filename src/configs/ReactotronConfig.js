4a88053761a4840eb16ea3dc84d92cea82878b13
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-11-27 11:05:16 +0700

    Dev-Setup-Redux: Update ReactotronConfig - config for real device

--- src/configs/ReactotronConfig.js
+++ src/configs/ReactotronConfig.js
@@ -1,12 +1,25 @@
 import Reactotron from 'reactotron-react-native';
 import {reactotronRedux as reduxPlugin} from 'reactotron-redux';
+import DeviceInfo from 'react-native-device-info';
+import {NativeModules} from 'react-native';
 
 import {APP_NAME} from '../constants';
 import {appConfigs} from './Configs';
 
-Reactotron.configure({
-  name: APP_NAME,
-});
+if (DeviceInfo.isEmulatorSync()) {
+  Reactotron.configure({
+    name: APP_NAME,
+  });
+} else {
+  // Config for real device
+  const host = NativeModules.SourceCode.scriptURL.split('://')[1].split(':')[0];
+  Reactotron.configure({
+    name: APP_NAME,
+    host: host ? host : '192.168.0.116',
+    port: '9090',
+  });
+}
+
 Reactotron.useReactNative();
 
 // add some more plugins for redux & redux-saga

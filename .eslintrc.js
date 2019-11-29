ab2a11dad43ae45a3ee61ae1ec5dbe7f6a0311b9
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-11-29 13:35:59 +0700

    Update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,7 @@ module.exports = {
   rules: {
     'react-native/no-inline-styles': 0,
     'prettier/prettier': 0,
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
   },
 };

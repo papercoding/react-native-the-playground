31ec2f350ee36e945588c6bf71bc2bfc1c819a52
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-02 11:53:37 +0700

    Dev-Setup-UI: Update CustomText component

--- src/components/CustomText/index.js
+++ src/components/CustomText/index.js
@@ -3,7 +3,7 @@ import {Text} from 'react-native';
 import {withTheme} from 'react-native-paper';
 
 function CustomText({children, theme, style}) {
-  return <Text style={[style, {color: theme.colors.text}]}>{children}</Text>;
+  return <Text style={[{color: theme.colors.text}, style]}>{children}</Text>;
 }
 
 export default withTheme(CustomText);

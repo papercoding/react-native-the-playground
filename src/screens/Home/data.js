5f7705397a3471b40c4a51b2fb42b1807adfb462
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-02 11:53:12 +0700

    Dev-Setup-UI: Create static data for Home screen

---
+++ src/screens/Home/data.js
@@ -0,0 +1,22 @@
+export const LIST_HOME_ITEM = [
+  {
+    id: 100,
+    title: 'UI Concepts',
+    imageSource: require('../../assets/images/ic_circle_paint_palette.png'),
+  },
+  {
+    id: 101,
+    title: 'Animations',
+    imageSource: require('../../assets/images/ic_circle_pencil.png'),
+  },
+  {
+    id: 102,
+    title: 'Showcase',
+    imageSource: require('../../assets/images/ic_circle_image.png'),
+  },
+  {
+    id: 103,
+    title: 'Have Fun',
+    imageSource: require('../../assets/images/ic_ballon.png'),
+  },
+];

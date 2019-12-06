503d3467d58ea06fec5ef54b10f864ad895ddced
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-06 18:21:05 +0700

    Dev - Setup - UI: Update HomeHeaderBackground - using LinearGradient as container view

--- src/screens/Home/HomeHeaderBackground.js
+++ src/screens/Home/HomeHeaderBackground.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {Animated, StyleSheet} from 'react-native';
 import {withTheme} from 'react-native-paper';
+import LinearGradient from 'react-native-linear-gradient';
+
 import {SCREEN_WIDTH} from '../../themes';
+
+const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
 function HomeHeaderBackground(props) {
   const {theme, style} = props;
+  const {colors} = theme;
   return (
-    <Animated.View style={[styles.container, {backgroundColor: theme.colors.primary}, style]} />
+    <AnimatedLinearGradient
+      start={{x: 0.0, y: 0.25}}
+      end={{x: 0.5, y: 1.0}}
+      colors={[colors.primaryLight, colors.primary, colors.primaryDark]}
+      style={[styles.container, style]}
+    />
   );
 }
 

b24cfed7e7724d00d6f282c28ca0ac3b30c1fc21
Tron Nguyen <johniusnguyen22297@gmail.com>
2019-12-06 18:17:26 +0700

    Dev - Setup - UI: Update HomeHeaderTitle - no need to use Animated View

--- src/screens/Home/HomeHeaderTitle.js
+++ src/screens/Home/HomeHeaderTitle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Animated} from 'react-native';
+import {View} from 'react-native';
 
 import CustomText from '../../components/CustomText';
 import TextStyles from '../../themes/TextStyles';
@@ -8,12 +8,12 @@ import {scale} from 'react-native-size-matters';
 const HomeHeaderTitle = props => {
   const {style} = props;
   return (
-    <Animated.View style={[{marginTop: scale(12)}, style]}>
+    <View style={[style]}>
       <CustomText style={[TextStyles.headline, {color: 'white'}]}>{'The Playground'}</CustomText>
       <CustomText style={[TextStyles.subheading, {color: 'white', marginTop: scale(4)}]}>
         {'Have fun with React Native'}
       </CustomText>
-    </Animated.View>
+    </View>
   );
 };
 

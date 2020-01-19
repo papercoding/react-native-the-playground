9290ecd63a71337fa9bb2c08b9dfcd96ed06af42
Tron Nguyen <johniusnguyen22297@gmail.com>
2020-01-19 16:19:08 +0700

    Update Colors.js

--- src/themes/Colors.js
+++ src/themes/Colors.js
@@ -1,7 +1,5 @@
-export const DarkMode = {
-  dark: true,
+const BaseMode = {
   colors: {
-    // primary
     primary: '#ffa259',
     primaryLight: '#ffd488',
     primaryDark: '#c8732c',
@@ -9,69 +7,55 @@ export const DarkMode = {
     // secondary
     secondary: '#62D7C6',
     onSecondary: '#212121',
-    // background
-    background: '#313131',
-    onBackground: '#ffffff',
-    // surface
-    surface: '#323232',
-    onSurface: '#ffffff',
-    // bottom tab bar
-    bottomTabBar: '#525252',
-    activeBottomTabBar: '#ffa259',
-    inactiveBottomTabBar: '#ffffff',
-    // header bar
-    headerBar: '#525252',
-    headerBarTintColor: '#ffffff',
-    // card
-    cardColor: '',
-    // button
-    buttonColor: '',
-    // dialog
-    dialogBackgroundColor: '',
-    // others
-    disabled: '',
-    placeholder: '',
-    backdrop: '',
-    dividerColor: '',
-    focusColor: '',
-    hoverColor: '',
-    highlightColor: '',
-    splashColor: '',
-    errorColor: '',
-    defaultStatusBar: '#000000',
-    text: '#ffffff',
+    baseDark: '#313131',
+    baseDark2: '#525252',
+    baseLight: '#edf7fa',
   },
 };
 
-export const LightMode = {
-  dark: false,
-  colors: {
-    // primary
-    primary: '#ffa259',
-    primaryLight: '#ffd488',
-    primaryDark: '#c8732c',
-    onPrimary: '#FFFFFF',
-    // secondary
-    secondary: '#62D7C6',
-    onSecondary: '#000000',
-    // background
-    background: '#6c5b7b',
-    onBackground: '#000000',
-    // surface
-    surface: '#FFFFFF',
-    onSurface: '#000000',
-    // bottom tab bar
-    bottomTabBar: '#ffa259',
-    activeBottomTabBar: '#FFFFFF',
-    inactiveBottomTabBar: '#c8732c',
-    // header bar
-    headerBar: '#ffa259',
-    headerBarTintColor: '#ffffff',
-    // others
-    disabled: '',
-    placeholder: '',
-    backdrop: '',
-    defaultStatusBar: '#c8732c',
-    text: '#121212',
-  },
-};
+export function getThemeMode(theme) {
+  return {
+    dark: theme === 'dark' ? true : false,
+    colors: {
+      // primary
+      primary: BaseMode.colors.primary,
+      primaryLight: BaseMode.colors.primaryLight,
+      primaryDark: BaseMode.colors.primaryDark,
+      onPrimary: BaseMode.colors.onPrimary,
+      // secondary
+      secondary: BaseMode.colors.secondary,
+      onSecondary: BaseMode.colors.onSecondary,
+      // background
+      background: theme === 'dark' ? BaseMode.colors.baseDark : BaseMode.colors.baseLight,
+      onBackground: theme === 'dark' ? '#ffffff' : '#000000',
+      // surface
+      surface: theme === 'dark' ? '#323232' : '#FFFFFF',
+      onSurface: theme === 'dark' ? '#ffffff' : '#000000',
+      // bottom tab bar
+      bottomTabBar: theme === 'dark' ? BaseMode.colors.baseDark2 : '#ffa259',
+      activeBottomTabBar: theme === 'dark' ? '#ffa259' : '#FFFFFF',
+      inactiveBottomTabBar: theme === 'dark' ? '#ffffff' : '#c8732c',
+      // header bar
+      headerBar: theme === 'dark' ? BaseMode.colors.baseDark2 : '#ffa259',
+      headerBarTintColor: theme === 'dark' ? '#ffffff' : '#ffffff',
+      // card
+      cardColor: theme === 'dark' ? BaseMode.colors.baseDark2 : '#FFFFFF',
+      // button
+      buttonColor: '',
+      // dialog
+      dialogBackgroundColor: '',
+      // others
+      disabled: '',
+      placeholder: '',
+      backdrop: '',
+      dividerColor: '',
+      focusColor: '',
+      hoverColor: '',
+      highlightColor: '',
+      splashColor: '',
+      errorColor: '',
+      defaultStatusBar: theme === 'dark' ? '#000000' : '#c8732c',
+      text: theme === 'dark' ? '#ffffff' : '#121212',
+    },
+  };
+}

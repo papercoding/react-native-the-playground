d3c5e9c2e6a9d0441c5f781293b8034afb4fc673
Paper Coding <johnnguyen2202@outlook.com>
2020-01-24 17:07:22 +0700

    Update Colors.js

--- src/themes/Colors.js
+++ src/themes/Colors.js
@@ -1,7 +1,30 @@
 import {DefaultTheme} from 'react-native-paper';
 
+/**
+ A theme usually contains the following properties:
+    dark (boolean): whether this is a dark theme or light theme.
+    mode ('adaptive' | 'exact'): color mode for dark theme (See Dark Theme).
+    roundness (number): roundness of common elements, such as buttons.
+    colors (object): various colors used throughout different elements.
+        primary - primary color for your app, usually your brand color.
+        accent - secondary color for your app which complements the primary color.
+        background - background color for pages, such as lists.
+        surface - background color for elements containing content, such as cards.
+        text - text color for content.
+        disabled - color for disabled elements.
+        placeholder - color for placeholder text, such as input placeholder.
+        backdrop - color for backdrops of various components such as modals.
+    fonts (object): various fonts used throughout different elements.
+        regular
+        medium
+        light
+        thin
+When creating a custom theme, you will need to provide all of these properties.
+ */
+
 const BaseMode = {
   colors: {
+    ...DefaultTheme.colors,
     primary: '#ffa259',
     primaryLight: '#ffd488',
     primaryDark: '#c8732c',
@@ -9,6 +32,7 @@ const BaseMode = {
     // secondary
     secondary: '#62D7C6',
     onSecondary: '#212121',
+    accent: '#f1c40f',
     baseDark: '#313131',
     baseDark2: '#525252',
     baseLight: '#e0e0e0',
@@ -19,8 +43,9 @@ export function getThemeMode(theme) {
   return {
     ...DefaultTheme,
     dark: theme === 'dark' ? true : false,
+    roundness: 2,
     colors: {
-      ...DefaultTheme.colors,
+      ...BaseMode.colors,
       // primary
       primary: BaseMode.colors.primary,
       primaryLight: BaseMode.colors.primaryLight,
@@ -29,6 +54,8 @@ export function getThemeMode(theme) {
       // secondary
       secondary: BaseMode.colors.secondary,
       onSecondary: BaseMode.colors.onSecondary,
+      // accent
+      accent: BaseMode.colors.accent,
       // background
       background: theme === 'dark' ? BaseMode.colors.baseDark : BaseMode.colors.baseLight,
       onBackground: theme === 'dark' ? '#ffffff' : '#000000',
@@ -37,7 +64,7 @@ export function getThemeMode(theme) {
       onSurface: theme === 'dark' ? '#ffffff' : '#000000',
       // bottom tab bar
       bottomTabBar: theme === 'dark' ? BaseMode.colors.baseDark2 : '#ffa259',
-      activeBottomTabBar: theme === 'dark' ? '#ffa259' : '#FFFFFF',
+      activeBottomTabBar: theme === 'dark' ? BaseMode.colors.primary : '#FFFFFF',
       inactiveBottomTabBar: theme === 'dark' ? '#ffffff' : '#c8732c',
       // header bar
       headerBar: theme === 'dark' ? BaseMode.colors.baseDark2 : '#ffa259',

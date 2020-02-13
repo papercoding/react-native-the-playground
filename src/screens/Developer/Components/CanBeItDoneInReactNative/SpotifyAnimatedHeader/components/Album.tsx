7b29e6a81ee54c3ed0f64543dcfe398c5bf5b7e7
Paper Coding <johnnguyen2202@outlook.com>
2020-02-13 23:45:49 +0700

    Add Spotify Animated Header Demo

---
+++ src/screens/Developer/Components/CanBeItDoneInReactNative/SpotifyAnimatedHeader/components/Album.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import {View, StyleSheet} from 'react-native';
+import Animated from 'react-native-reanimated';
+import {Album} from './Model';
+import Content from './Content';
+import Cover from './Cover';
+interface AlbumProps {
+  album: Album;
+}
+
+const {Value} = Animated;
+
+export default ({album}: AlbumProps) => {
+  const y = new Value(0);
+  return (
+    <View style={styles.container}>
+      <Cover {...{y, album}} />
+      <Content {...{y, album}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+});

db66e929d97100995feefb03a9069c84365c0856
Trung Nguyen Quoc <johnnguyen2202@outlook.com>
2020-02-14 17:58:39 +0700

    Update script env

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ import './src/configs/ReactotronConfig';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './src/App';
+import Config from 'react-native-config';
+
+if (__DEV__) {
+  console.tron.log(Config.ENV);
+}
 
 AppRegistry.registerComponent(appName, () => App);

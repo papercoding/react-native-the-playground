dd18eb838932a650f2c538ebf593310e71e2507b
Trung Nguyen Quoc <johnnguyen2202@outlook.com>
2020-07-26 11:25:51 +0700

    Add reduxjs toolkit lib

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,8 @@
-import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import AsyncStorage from '@react-native-community/async-storage';
+import Reactotron from 'reactotron-react-native';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import {persistReducer, persistStore} from 'redux-persist';
 import thunk from 'redux-thunk';
-
-import Reactotron from 'reactotron-react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-
 import * as rootReducer from '../reducers';
 
 const combinedReducer = combineReducers(rootReducer);
@@ -19,11 +17,15 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, combinedReducer);
 
-let store = createStore(
+const store = createStore(
   persistedReducer,
-  compose(applyMiddleware(thunk), Reactotron.createEnhancer()),
+  compose(
+    applyMiddleware(thunk),
+    Reactotron.createEnhancer(),
+  ),
 );
-let persistor = persistStore(store);
+
+const persistor = persistStore(store);
 
 store.subscribe(() => {
   console.tron.log('Subscribe Store:', store.getState());

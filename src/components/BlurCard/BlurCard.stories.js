5b47a48e305219cc42998f3f4af975d8d3b46c8c
Trung Nguyen Quoc <johnnguyen2202@outlook.com>
2020-08-01 12:44:41 +0700

    Refactor

--- src/components/BlurCard/BlurCard.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import BlurCard from './BlurCard';
-
-import {storiesOf} from '@storybook/react-native';
-import CenterView from '../../../storybook/stories/CenterView';
-import {action} from '@storybook/addon-actions';
